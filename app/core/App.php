b65035091c8449cf93e79dead89bc84690886132
Kittiphong Xayasane <kxayasan@gmail.com>
2014-10-16 12:52:45 -0400

    added some controller functionality

--- app/core/App.php
+++ app/core/App.php
@@ -13,8 +13,32 @@ class App
  	//Do things when we load the page 
 	public function __construct()
 	{
+		$url = $this->parseUrl();
 
-		print_r($this->parseUrl());
+		if(file_exists('../app/controllers/' . $url[0] . '.php'))
+		{
+			
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+		
+
+		require_once '../app/controllers/' . $this->controller . '.php';
+		
+		$this->controller = new $this->controller;
+
+		if(isset($url[1]))
+		{
+			if(method_exists($this->controller, $url[1]))
+			{
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		$this->params = $url ? array_values($url) : [];
+
+		call_user_func_array([$this->controller, $this->method], $this->params);
 	}
 
 	public function parseUrl()
