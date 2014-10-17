a146b6ca7a6fe81e35f3ab166e6be563c315b18d
Kittiphong Xayasane <kxayasan@gmail.com>
2014-10-17 17:42:01 -0400

    rendering views and outputting data

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,7 +6,9 @@ class Home extends Controller
 	{
 		$user = $this->model('User');
 		$user->name = $name;
-		echo $user->name;
+
+		//path
+		$this->view('home/index', ['name' => $user->name]);
 	}
 
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,9 +3,13 @@
 //Controller loads in models and render views
 class Controller
 {
-	public function model($model)
+	protected function model($model)
 	{
 		require_once '../app/models/' . $model . '.php';
 		return new $model();
+	}
+	public function view($view, $data = [])
+	{
+		require_once '../app/views/' . $view . '.php';
 	}
 }
