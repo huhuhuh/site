34d4eab46c6af22ab565cad892fb1411e27d515f
automata <vicecao@gmail.com>
2014-10-19 23:25:03 -0400

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -2,5 +2,17 @@
 
 class User
 {
-	public $name;
+	
+	protected $name;
+	
+	
+	public function getName() {
+		return $this->name;
+	}
+	
+	public function setName($name) {
+		$this->name = $name
+	}
+	
+	
 }
